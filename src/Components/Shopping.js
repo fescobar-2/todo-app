f14609fd70188d9465cb6da892e977dd32425ad1
Fede Escobar <fescobar_2@hotmail.com>
2021-09-03 11:17:05 -0400

    Added Routes and README description

---
+++ src/Components/Shopping.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+export default function Shopping() {
+    return(
+        <h1>Hola!</h1>
+    )
+}
--- src/MainRouter.js
+++ src/MainRouter.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Switch} from 'react-router';
 import Navigation from "./Components/Navigation";
 import Home from "./Components/Home";
+import Shopping from "./Components/Shopping";
 
 export default function MainRouter() {
   return(<div>
@@ -9,6 +10,9 @@ export default function MainRouter() {
     <Switch>
       <Route exact path="/">
         <Home/>
+      </Route>
+      <Route path="/shopping">
+        <Shopping/>
       </Route>
     </Switch>
     </div>
