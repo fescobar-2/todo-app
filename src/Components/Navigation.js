07480fbd0c30b494abe53a3cffe2937edca1ae2b
Fede Escobar <fescobar_2@hotmail.com>
2021-09-09 16:42:40 -0400

    Added Links instead of Nav.Links

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,5 +1,5 @@
 import React from "react";
-//import Navbar from "react-bootstrap/Navbar";
+import Navbar from "react-bootstrap/Navbar";
 //import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container";
 import { Link } from "react-router-dom";
@@ -7,10 +7,12 @@ import { Link } from "react-router-dom";
 export default function Navigation() {
 	return(
 		<div>
+      <Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
 		  <Container>
 			  <Link to ="/">Home</Link>
 			  <Link to="/login">Login</Link>
 			</Container>
+      </Navbar>
 		</div>
 		// <Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
 		// 	<Container>
