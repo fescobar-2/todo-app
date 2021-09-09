0a9a5b25e481afbde2cda83f0d3ae1d23e68fd30
Fede Escobar <fescobar_2@hotmail.com>
2021-09-09 11:55:45 -0400

    Added Link from react-router instead of react-bootstrap

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,24 +1,29 @@
 import React from "react";
-import  Navbar from "react-bootstrap/Navbar";
+import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import Container from "react-bootstrap/Container"
+import { Link } from "react-router-dom";
 
 export default function Navigation() {
 	return(
-		<Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
-			<Container>
-			<Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
-			<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-			<Navbar.Collapse id="responsive-navbar-nav">
-				<Nav className="me-auto">
-					<Nav.Link href="/">Shopping List</Nav.Link>
-					<Nav.Link href="/shopping">Todo List</Nav.Link>
-				</Nav>
-				<Nav className="me-auto">
-					<Nav.Link href="/login">Login</Nav.Link>
-				</Nav>
-			</Navbar.Collapse>
-			</Container>
-		</Navbar>
+		<div>
+			<Link to="/">Home</Link>
+			<Link to="/login">Login</Link>
+		</div>
+		// <Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
+		// 	<Container>
+		// 	<Navbar.Brand href="/">React-Bootstrap</Navbar.Brand>
+		// 	<Navbar.Toggle aria-controls="responsive-navbar-nav" />
+		// 	<Navbar.Collapse id="responsive-navbar-nav">
+		// 		<Nav className="me-auto">
+		// 			<Nav.Link to="/">Shopping List</Nav.Link>
+		// 			<Nav.Link to="/shopping">Todo List</Nav.Link>
+		// 		</Nav>
+		// 		<Nav className="me-auto">
+		// 			<Nav.Link to="/login">Login</Nav.Link>
+		// 		</Nav>
+		// 	</Navbar.Collapse>
+		// 	</Container>
+		// </Navbar>
 	)
 }
