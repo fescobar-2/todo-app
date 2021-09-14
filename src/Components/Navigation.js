42b81bb8923530944eeb61a808e8a384966b5634
Fede Escobar <fescobar_2@hotmail.com>
2021-09-14 11:24:30 -0400

    Added style to navigation and fixed routing

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,20 +1,26 @@
 import React from "react";
-//import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import Container from "react-bootstrap/Container";
-import { NavLink } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function Navigation() {
 	return(
 		<div>
-        <Container>
-          <Nav className="me-auto">
-            <NavLink to ="/">Home</NavLink>
-          </Nav>
-          <Nav className="me-auto">
-            <NavLink to ="/login">Login</NavLink>
-          </Nav>
-        </Container>
+        <Navbar bg="light" expand="lg">
+          <LinkContainer to="/">
+            <Navbar.Brand>Shopping List</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="mr-auto">
+                <LinkContainer to="/">
+                  <Nav.Link>Home</Nav.Link>
+                </LinkContainer>
+                <LinkContainer to="/login">
+                  <Nav.Link>Login</Nav.Link>
+                </LinkContainer>
+              </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 		</div>
 		// <Navbar collapseOnSelect expand="md" bg="primary" variant="dark">
 		// 	<Container>
